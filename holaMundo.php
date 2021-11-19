3d5170eb4d9368308dc958750e9420bb63aca9e3
angel trejo <angel_t_r@hotmail.com>
2021-11-18 21:21:13 -0600

    segunda version del script

--- holaMundo.php
+++ holaMundo.php
@@ -3,12 +3,6 @@ echo 'Hola Mundo!';
 echo'<br>';
 echo 'primera version del script';
 echo'<br>';
-echo 'segunda version del script';
-echo'<br>';
-echo 'tercera version del script';
-echo'<br>';
-echo 'cuarta version del script';
-echo'<br>';
-echo 'quinta version del script';
+
 
 ?>
